6b3d6c23598137423aeda39a4fe46592f6023aab
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-29 23:23:29 +0600

    Program done..

--- ThreadExampleStaticMethod/Program.cs
+++ ThreadExampleStaticMethod/Program.cs
@@ -24,7 +24,12 @@ namespace ThreadExampleStaticMethod
             Thread t1 = new Thread(new ThreadStart(MyThread.Thread1));
             Thread t2 = new Thread(new ThreadStart(MyThread.Thread1));
 
+            Console.WriteLine("Thread t1 has been called: ");
+            
             t1.Start();
+
+            Console.WriteLine("Thread t2 has been called: ");
+
             t2.Start();
         }
     }
